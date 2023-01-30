4227c0de67f9ad865c226367704b804230c3e244
Marco Milanesi <kpanic@gmail.com>
2023-01-30 19:33:19 +0100

    Rename typo

--- lib/application.ex
+++ lib/application.ex
@@ -6,7 +6,7 @@ defmodule Gigex.Application do
   def start(_type, _args) do
     children = [Gigex.Cache]
 
-    opts = [strategy: :one_for_one, name: Forecastr.Supervisor]
+    opts = [strategy: :one_for_one, name: Gigex.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
